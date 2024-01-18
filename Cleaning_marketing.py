17312323113b24392e06fcc806880e947d469fa0
mailsyed078 <mailsyed078>
2024-01-18 07:18:50 +0000

    cleaned_data

---
+++ Cleaning_marketing.py
@@ -0,0 +1,75 @@
+# Databricks notebook source
+file_path = '/FileStore/Marketing_Automation_csv.csv'
+
+# COMMAND ----------
+
+raw_df = spark.read.csv(file_path, header="true", inferSchema="true", multiLine="true", escape='"')
+display(raw_df)
+
+# COMMAND ----------
+
+raw_df.count(),len(raw_df.columns)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Dropping columns
+
+# COMMAND ----------
+
+raw_df.columns
+
+# COMMAND ----------
+
+raw_df = raw_df.drop('Device_Type')
+raw_df.columns
+
+# COMMAND ----------
+
+raw_df.select('_c9').distinct().display()
+
+# COMMAND ----------
+
+raw_df = raw_df.drop('_c9')
+raw_df.columns
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### checking and rectifying datatypes
+
+# COMMAND ----------
+
+raw_df.printSchema()
+
+# COMMAND ----------
+
+### all the datatypes are correct in their places
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Stats
+
+# COMMAND ----------
+
+dbutils.data.summarize(raw_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Working on duplictes
+
+# COMMAND ----------
+
+raw_df.dropDuplicates().count()
+
+# COMMAND ----------
+
+df = raw_df.dropDuplicates()
+df.count()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### there are no outleirs and null values
