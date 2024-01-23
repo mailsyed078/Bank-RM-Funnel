c1f2d8276ad1ecd24b9938b01258da578307de0e
mailsyed078 <mailsyed078>
2024-01-23 10:47:49 +0000

    changes

--- XGBoost Model Building.py
+++ XGBoost Model Building.py
@@ -26,7 +26,7 @@ df.display()
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ### Random Forest Classification 
+# MAGIC ### XGB Classification 
 
 # COMMAND ----------
 
@@ -40,10 +40,6 @@ pdf = df.toPandas()
 X = pdf.drop('Conversion_Status',axis = 1)
 y = pd.get_dummies(pdf['Conversion_Status'],drop_first=True)
 
-
-# COMMAND ----------
-
-X.columns
 
 # COMMAND ----------
 
@@ -61,18 +57,19 @@ enc_df = enc_tar.fit_transform(X,y)
 
 # COMMAND ----------
 
-enc_df
-
-# COMMAND ----------
-
 concated_df = pd.concat([enc_df,y],axis=1).rename(columns = {'Not Converted':'Conversion_Status'})
 spark_df = spark.createDataFrame(concated_df)
 spark_df.display()
 
 # COMMAND ----------
 
+!pip install sparkxgb
+
+# COMMAND ----------
+
 ## Importing required libraries
-from pyspark.ml.classification import RandomForestClassifier
+import sparkxgb
+from sparkxgb.xgboost import XGBoostClassifier
 from pyspark.ml.feature import VectorAssembler
 
 # COMMAND ----------
@@ -89,61 +86,50 @@ train, val, test = spark_df.randomSplit([0.6,0.2,0.2])
 
 # creating model and Pipeline
 from pyspark.ml import Pipeline
-rf = RandomForestClassifier(featuresCol='inp_features',
-                        labelCol='Conversion_Status')
-stages = [vc,rf]
-pipeline_rf = Pipeline(stages = stages)
+xgb = XGBoostClassifier(objective="binary:logistic",
+                        featuresCol="inp_features",
+                        labelCol="Conversion_Status")
+stages = [vc,xgb]
+pipeline_xgb = Pipeline(stages = stages)
 
 # COMMAND ----------
 
 # training the model with default hyperparameter
-pipe_model = pipeline_rf.fit(train)
-preds_rf = pipe_model.transform(val)
-preds_rf.display()
-
-# COMMAND ----------
-
-import pandas as pd
-features_imp = pipe_model.stages[-1].featureImportances
-features_imp
-
-# COMMAND ----------
-
-import pandas as pd
-features_df = pd.DataFrame(list(zip(vc.getInputCols(),features_imp)),columns=["feature","importance"]).sort_values(by='importance',ascending=False)
-features_df
-
-# COMMAND ----------
-
-import seaborn as sns
-sns.barplot(x=features_df['importance'],y=features_df['feature']) 
+pipe_model = pipeline_xgb.fit(train)
+preds_xgb = pipe_model.transform(val)
+preds_xgb.display()
 
 # COMMAND ----------
 
 # Model evaluation
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 evaluator = MulticlassClassificationEvaluator(metricName ='f1',labelCol ='Conversion_Status')
-score = evaluator.evaluate(preds_rf)
+score = evaluator.evaluate(preds_xgb)
 print(score)
 
 # COMMAND ----------
 
-help(RandomForestClassifier)
+help(XGBoostClassifier)
 
 # COMMAND ----------
 
-# DBTITLE 1,Hyperparameter Tuning for Random Forest model
+# DBTITLE 1,Hyperparameter Tuning for XGB model
 # defining objective_function
 import mlflow
 
 def objective_fn(params):
     # setting the hyperparamters
     max_depth = params["max_depth"]
-    num_trees = params["num_trees"]
+    alpha = params["alpha"]
+    eta = params["eta"]
+    eval_metric = params["eval_metric"]
 
-    with mlflow.start_run(run_name = "lr_model_hyp") as run:
-        estimator = pipeline_rf.copy({rf.maxDepth:max_depth,
-                                      rf.numTrees:num_trees})
+
+    with mlflow.start_run(run_name = "xgb_model_hyp") as run:
+        estimator = pipeline_xgb.copy({xgb.maxDepth:max_depth,
+                                      xgb.alpha:alpha,
+                                      xgb.eta:eta,
+                                      xgb.evalMetric:eval_metric})
         model = estimator.fit(train)
         preds = model.transform(val)
         score = evaluator.evaluate(preds)
@@ -155,10 +141,16 @@ def objective_fn(params):
 
 # defining the search space
 from hyperopt import hp
+max_depth = [5,7,10]
+alpha = [0.01,0.05,0.1]
+eta = [0.1,0.2,0.3]
+eval_metric = ['error','logloss']
 
 search_space = {
-    'max_depth': hp.quniform('max_depth',5,15,3), 
-    'num_trees': hp.quniform('num_trees',100,500,10)  
+    'max_depth': hp.choice('max_depth',max_depth), 
+    'alpha': hp.choice('alpha',alpha),
+     'eta': hp.choice('eta',eta),
+     'eval_metric':hp.choice('eval_metric',eval_metric)  
 }
 
 
@@ -169,45 +161,43 @@ from hyperopt import hp,tpe, Trials, fmin
 best_params = fmin(fn=objective_fn,
                    space = search_space,
                    algo = tpe.suggest,
-                   max_evals = 9,
+                   max_evals = 54,
                    trials = Trials())
 best_params
 
 # COMMAND ----------
 
 # Retraining the LR model with the best parameter
-with mlflow.start_run(run_name = 'LR_final_model') as run:
+with mlflow.start_run(run_name = 'xgb_final_model') as run:
     mlflow.autolog()
 
-    best_max_iter = best_params['max_iter']
-    best_threshold = best_params['threshold']
+    best_max_depth = max_depth[best_params['max_depth']]
+    best_alpha = alpha[best_params['alpha']]
+    best_eta = eta[best_params['eta']]
+    best_eval_metric = eval_metric[best_params['eval_metric']]
 
     # creating pipeline
-    estimator = pipeline_lr.copy({lr.maxIter:best_max_iter, lr.threshold:best_threshold})
+    estimator = pipeline_xgb.copy({xgb.maxDepth:best_max_depth,
+                                      xgb.alpha:best_alpha,
+                                      xgb.eta:best_eta,
+                                      xgb.evalMetric:best_eval_metric})
     model = estimator.fit(train)
     preds = model.transform(val)
     score = evaluator.evaluate(preds)
+    mlflow.spark.log_model(model,'model')
     
-    # logging the parameters and metrics
-    
-    mlflow.log_metrics('f1_score',score) 
-    mlflow.log_param('best_max_iter',best_max_iter)
-    mlflow.log_param('best_threshold',best_threshold)
-
+    # logging the parameters and metric
+    mlflow.log_metric('f1_score', score) 
+    mlflow.log_param('best_max_depth', best_max_depth)
+    mlflow.log_param('best_alpha', best_alpha)
+    mlflow.log_param('best_eta', best_eta)
+    mlflow.log_param('best_eval_metric', best_eval_metric)
 
 # COMMAND ----------
 
 # DBTITLE 1,Prediction on test data.
-import mlflow
-
-run_id = run.info.run_id
-logged_model = f'runs:/{run_id}/model'
-
-#Loading model
-loaded_model = mlflow.spark.load_model(logged_model)
-
-# Performing inference from the loaded model
-predictions = loaded_model.transform(test)
+# Performing inference from the trained model where model ouput 0 states 'Converted' and 1 states 'Not Converted'
+predictions = model.transform(test)
 predictions.display()
 
 # COMMAND ----------
@@ -215,3 +205,19 @@ predictions.display()
 #accuracy on test data:
 score = evaluator.evaluate(predictions)
 print(score)
+
+# COMMAND ----------
+
+#calculating the ratio of conversion
+ratio_df =  predictions.groupBy('Prediction').count().toPandas()
+ratio_df
+
+# COMMAND ----------
+
+#calculating the effectiveness
+effectiveness = (ratio_df['count'][0]/(ratio_df['count'][0]+ratio_df['count'][1]))*100
+print(f"As per the given data the effectiveness of the funnel is: {effectiveness}")
+
+# COMMAND ----------
+
+
